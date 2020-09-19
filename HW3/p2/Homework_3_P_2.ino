b4787b9177d31632798ebc16da01d764f44fc21f
frozen616 <40275175+frozen616@users.noreply.github.com>
2020-09-19 15:50:43 -0600

    Add files via upload

---
+++ HW3/p2/Homework_3_P_2.ino
@@ -0,0 +1,91 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "mympu6050.h"
+#include "report.h"
+
+
+
+
+
+
+
+
+bool mysetup(MyMPU6050& mpu)
+{
+  uint8_t byteRead;
+  Serial.begin(38400);
+  Wire.begin();                 // Initiate Wire library, join I2C bus as master
+  delay(300);
+
+  byteRead = mpu.testConnection();
+  if(byteRead != 0x68)
+  {
+    Serial.println(F("success communicating with MPU 6050"));
+    return false;
+  }
+
+  mpu.initialize();
+  
+  return true;
+}
+
+
+
+
+
+
+
+
+int main(void)
+{
+  MyMPU6050 mpu(MPU6050_ADDRESS_AD0_LOW);
+  SensorData sd;
+  mean_values mv;
+  offset_values ov;
+  uint8_t bytesRead;
+  
+  init();         // Arduino function, config timers and other hardware
+
+  
+  if(mysetup(mpu) == false){
+    Serial.println("setup failed, entering infinite do-nothing while loop");
+    while(1)
+    {
+      delay(1000);
+    }
+  }
+
+  reportPowerState(mpu);
+  delay(1000);
+  reportConfigState(mpu);
+  delay(1000);
+  reportAccelConfigState(mpu);
+  delay(1000);
+  reportGyroConfigState(mpu);
+  Serial.println("");
+  reportRegisterOffsets(mpu);
+  delay(1000);
+  bytesRead = mpu.readAllData(&sd);
+  mpu.mean_calc(&sd, &mv, &ov);
+  mpu.calibration(&sd, &ov, &mv);
+
+  while(1)
+  {
+    bytesRead = mpu.readAllData(&sd);
+    if(bytesRead == MPU6050_ALL_MEASUREMENT_BYTES)
+    {
+ 
+      printUnscaledData(&sd, &ov);
+      printScaledData(&sd, &ov);
+    }
+    else
+    {
+      Serial.print("bytes read: ");
+      Serial.println(bytesRead);
+    }
+    delay(500);
+    
+  }
+  
+  return 0;
+}
---
+++ HW3/p2/report.h
@@ -0,0 +1,44 @@
+#ifndef REPORT_H_INCLUDED
+#define REPORT_H_INCLUDED
+
+#include "mympu6050.h"
+
+/*
+ * Uses serial library to print power management 1
+ * register value.
+ */
+void reportPowerState(MyMPU6050& mpu);
+
+/*
+ * Uses serial library to print configuration
+ * register value.
+ */
+void reportConfigState(MyMPU6050& mpu);
+
+/*
+ * Uses serial library to print accelerometer
+ * configuration register value.
+ */
+void reportAccelConfigState(MyMPU6050& mpu);
+
+/*
+ * Uses serial library to print gyroscope
+ * configuration register value.
+ */
+void reportGyroConfigState(MyMPU6050& mpu);
+
+/* Uses serial library to print accelerometer
+ *  and gyroscope offset register values
+ * 
+ */
+void reportRegisterOffsets(MyMPU6050& mpu);
+
+
+void printUnscaledData(const SensorData *sd, const offset_values *ov);
+
+void printScaledData(const SensorData *sd, const offset_values *ov);
+
+
+
+
+#endif 
